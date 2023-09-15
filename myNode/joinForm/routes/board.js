79e3b25ac58272cc2c9498e5a65d79a0989781f9
bbangjun <lucid9dawn@gmail.com>
2023-09-15 13:44:34 +0900

    feat(61p board.js): 글쓰기 기능 구현을 위한 소스코드 추가

--- myNode/joinForm/routes/board.js
+++ myNode/joinForm/routes/board.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var listController = require('../controllers/listController');
+var writeController = require('../controllers/writeController'); // 61p
 
 router.get('/', listController.getListFirst);
 router.get('/list/:idx', listController.getList);
+router.get('/write', writeController.writeForm);
+router.post('/write', writeController.writeData); // 61p
 
 module.exports=router;
