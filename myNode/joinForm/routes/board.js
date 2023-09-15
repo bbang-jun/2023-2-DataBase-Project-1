461f1dfe6831e4bdb3af4bebac0a66843a995cdb
bbangjun <lucid9dawn@gmail.com>
2023-09-15 17:38:44 +0900

    feat(74p /routes/board.js): 업데이트 기능 구현을 위해 board.js에 코드 추가

--- myNode/joinForm/routes/board.js
+++ myNode/joinForm/routes/board.js
@@ -3,11 +3,15 @@ var router = express.Router();
 var listController = require('../controllers/listController');
 var writeController = require('../controllers/writeController'); // 61p
 var readController = require('../controllers/readController'); // 68p
+var updateController = require('../controllers/updateController'); // 74p
+const multer =require('multer'); // 74p
 
 router.get('/', listController.getListFirst);
 router.get('/list/:idx', listController.getList);
 router.get('/write', writeController.writeForm); // 61p
 router.post('/write', writeController.writeData); // 61p
 router.get('/read/:idx', readController.readData); // 68p
+router.get('/update', updateController.updateForm); // 74p
+router.post('/update', multer().none(), (req,res)=>{updateController.updateData(req, res)}); // 74p
 
 module.exports=router;
