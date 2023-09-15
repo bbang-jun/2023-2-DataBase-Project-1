1c8bb9319e2ae8fa432e76452cf254a073ddb101
bbangjun <lucid9dawn@gmail.com>
2023-09-15 13:07:23 +0900

    feat(58p /models/listModel.js): listModel.js 생성

---
+++ myNode/joinForm/models/listModel.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    connectionLimit: 5,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    databse: 'tutorial'
+});
+
+module.exports={getList(callback){
+    connection.query('SELECT idx, creator_id, title, hit FROM board', (err, rows, fields)=>{
+        if(err) throw err;
+        callback(rows);
+    });
+}}
