4bf8ec0bb18e18d8ef5daca68fd380b82919e83d
bbangjun <lucid9dawn@gmail.com>
2023-09-15 14:31:02 +0900

    feat(70p /models/readModel.js): 업데이트 기능 구현을 위해 readModel.js에 로직 추가

---
+++ myNode/joinForm/models/readModel.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    connectionLimit: 5,
+    host: 'localhost',
+    user: 'root',
+    password: '@',
+    database: 'tutorial'
+});
+
+module.exports={getData(idx,callback){
+        connection.query('SELECT idx, creator_id, title, content, hit FROM board WHERE idx=?;', idx,(err,row,fields)=>{
+            if(err) throw err;
+            callback(row);
+        });
+    }
+}
