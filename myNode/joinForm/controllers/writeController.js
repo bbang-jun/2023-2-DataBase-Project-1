4502de486afad8e5aa75df4da484290788a06a1c
bbangjun <lucid9dawn@gmail.com>
2023-09-18 17:15:17 +0900

    feat(write): 게시글 작성 시 이미지 추가 기능 구현 및 로컬에 이미지 저장 확인 완료

--- myNode/joinForm/controllers/writeController.js
+++ myNode/joinForm/controllers/writeController.js
@@ -5,12 +5,24 @@ exports.writeForm=(req,res)=>{
     res.render('write',{title: "게시판 글 쓰기"});
 }
 exports.writeData=(req,res)=>{
-    var creator_id = req.body.creator_id;
-    var title = req.body.title;
-    var content = req.body.content;
-    var passwd = req.body.passwd;
-    var datas = [creator_id, title, content, passwd];
-    writeModel.insertData(datas,()=>{
-        res.redirect('/board');
+    // 이미지 업로드 미들웨어 호출
+    writeModel.uploadImage(req, res, function (err) {
+        if (err) {
+            // 업로드 중 오류가 발생한 경우 처리
+            console.error(err);
+            return res.status(500).send("이미지 업로드 오류");
+        }
+        
+        // 이미지 업로드 성공한 경우
+        var creator_id = req.body.creator_id;
+        var title = req.body.title;
+        var content = req.body.content;
+        var passwd = req.body.passwd;
+        var image_path = req.file ? req.file.path : null; // 이미지 경로 가져오기
+
+        var datas = [creator_id, title, content, passwd, image_path];
+        writeModel.insertData(datas, () => {
+            res.redirect('/board');
+        });
     });
 };
--- myNode/joinForm/models/writeModel.js
+++ myNode/joinForm/models/writeModel.js
@@ -1,18 +1,35 @@
 var mysql = require('mysql');
+var multer = require('multer');
+var path = require('path');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images'); // 이미지를 저장할 디렉터리 경로 설정
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname); // 파일 이름 설정
+    }
+});
+
+var upload = multer({ storage: storage });
+
 var connection = mysql.createConnection({
     connectionLimit: 5,
     host: 'localhost',
     user: 'root',
-    password: '@',
+    password: '@mrkdqudwns1',
     database: 'tutorial'
 });
 
-module.exports={insertData(datas,callback){
-        var sql="INSERT INTO board(creator_id, title, content, passwd) VALUES(?,?,?,?)";
-        connection.query(sql,datas,function(err,rows){
-         if(err) console.error("err : "+err);
-         console.log("rows : " + JSON.stringify(rows));
-         callback();
+module.exports = {
+    insertData: function (datas, callback) {
+        var sql = "INSERT INTO board(creator_id, title, content, passwd, image_path) VALUES(?,?,?,?,?)";
+        connection.query(sql, datas, function (err, rows) {
+            if (err) console.error("err : " + err);
+            console.log("rows : " + JSON.stringify(rows));
+            callback();
         });
-    }
-}
+    },
+
+    uploadImage: upload.single('image') // 이미지 업로드 미들웨어 설정
+};
