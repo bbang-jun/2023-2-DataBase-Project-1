f67bcbb03c9059578f2581f16a82b79da4de6f26
bbangjun <lucid9dawn@gmail.com>
2023-10-01 03:26:00 +0900

    docs: 보고서 작성 중 63p까지 주석 작성 완료

--- myNode/joinForm/models/writeModel.js
+++ myNode/joinForm/models/writeModel.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var mysql = require('mysql'); // 63p
 var multer = require('multer');
 var path = require('path');
 
@@ -16,26 +16,25 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-var connection = mysql.createConnection({
-    connectionLimit: 5,
-    host: 'localhost',
-    user: 'root',
-    password: '@mrkdqudwns1',
-    database: 'tutorial'
-});
+var connection = mysql.createConnection({ // 63p
+    connectionLimit: 5, // 63p
+    host: 'localhost', // 63p
+    user: 'root', // 63p
+    password: '@mrkdqudwns1', // 63p
+    database: 'tutorial' // 63p
+}); // 63p
 
-module.exports = {
-    insertData: function (datas, callback) {
+module.exports = {insertData: function (datas, callback) { // 63p
         // datas 배열의 다섯 번째 요소는 이미지 파일 이름이 됩니다.
         // 여기에서 "public\\"를 제거하여 저장합니다.
         datas[4] = datas[4].replace("public\\", "");
         console.log("=====================" + datas[0] +" "+datas[1]+" "+datas[2]+ " " +datas[3] +" "+ datas[4] )
         var sql = "INSERT INTO board(creator_id, title, content, passwd, image_path) VALUES(?,?,?,?,?)";
-        connection.query(sql, datas, function (err, rows) {
-            if (err) console.error("err : " + err);
-            console.log("rows : " + JSON.stringify(rows));
-            callback();
-        });
+        connection.query(sql, datas, function (err, rows) { // 63p
+            if (err) console.error("err : " + err); // 63p
+            console.log("rows : " + JSON.stringify(rows)); // 63p
+            callback(); // 63p
+        }); // 63p
     },
 
     uploadImage: upload.single('image') // 이미지 업로드 미들웨어 설정
