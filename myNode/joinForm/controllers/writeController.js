ba31494c43d326a40789cf7365c101999a97ff61
bbangjun <lucid9dawn@gmail.com>
2023-10-01 03:20:52 +0900

    docs: 보고서 작성 중 62p까지 주석 작성 완료

--- myNode/joinForm/controllers/writeController.js
+++ myNode/joinForm/controllers/writeController.js
@@ -1,10 +1,10 @@
-var writeModel=require('../models/writeModel');
-var express=require('express');
+var writeModel=require('../models/writeModel'); // 62p
+var express=require('express'); // 62p
 
-exports.writeForm=(req,res)=>{
-    res.render('write',{title: "게시판 글 쓰기"});
+exports.writeForm=(req,res)=>{ // 62p
+    res.render('write',{title: "게시판 글 쓰기"}); // 62p
 }
-exports.writeData=(req,res)=>{
+exports.writeData=(req,res)=>{ // 62p
     // 이미지 업로드 미들웨어 호출
     writeModel.uploadImage(req, res, function (err) {
         if (err) {
@@ -14,17 +14,17 @@ exports.writeData=(req,res)=>{
         }
         
         // 이미지 업로드 성공한 경우
-        var creator_id = req.body.creator_id;
-        var title = req.body.title;
-        var content = req.body.content;
-        var passwd = req.body.passwd;
+        var creator_id = req.body.creator_id; // 62p
+        var title = req.body.title; // 62p
+        var content = req.body.content; // 62p
+        var passwd = req.body.passwd; // 62p
         var image_path = req.file ? req.file.path : null; // 이미지 경로 가져오기
 
-        var datas = [creator_id, title, content, passwd, image_path];
+        var datas = [creator_id, title, content, passwd, image_path]; // 62p
         console.log("writeController data : ", datas);
         console.log(JSON.stringify(req.body));
-        writeModel.insertData(datas, () => {
-            res.redirect('/board');
-        });
+        writeModel.insertData(datas, () => { // 62p
+            res.redirect('/board'); // 62p
+        }); // 62p
     });
-};
+}; // 62p
