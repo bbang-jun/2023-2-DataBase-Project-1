d9100e02d8bee2d5734b3c0e2a1a3eac660c7e54
bbangjun <lucid9dawn@gmail.com>
2023-10-01 22:28:26 +0900

    docs: 보고서 작성 중 75p까지 주석 작성 완료

--- myNode/joinForm/controllers/updateController.js
+++ myNode/joinForm/controllers/updateController.js
@@ -1,42 +1,37 @@
-var updateModel=require('../models/updateModel');
-var express=require('express');
-var url = require('url');
+var updateModel=require('../models/updateModel'); // 75p
+var express=require('express'); // 75p
+var url = require('url'); // 75p
 
-exports.updateForm=(req, res, next)=>{
-    var queryData = url.parse(req.url, true).query;
-    var idx = queryData.idx;
-    updateModel.getData(idx, (row)=>{
-        console.log('update에서 1개 글 조회 결과 확인 : ',row);
-        res.render('update',{title: "글 수정", row: row[0]});
+exports.updateForm=(req, res, next)=>{ // 75p
+    var queryData = url.parse(req.url, true).query; // 75p
+    var idx = queryData.idx; // 75p
+    updateModel.getData(idx, (row)=>{ // 75p
+        console.log('update에서 1개 글 조회 결과 확인 : ',row); // 75p
+        res.render('update',{title: "글 수정", row: row[0]}); // 75p
     });
 
 }
 
-exports.updateData = (req, res) => {
+exports.updateData = (req, res) => { // 75p
 
     // 이미지가 업로드되었을 때 이미지 경로를 포함하여 SQL 쿼리를 실행합니다.
         updateModel.uploadImage(req, res, function(err) {
-            // if (result.affectedRows == 0) {
-            //     res.send("<script>alert('패스워드가 일치하지 않거나, 잘못된 요청으로 인해 변경되지 않았습니다.');history.back();</script>");
-            // } else {
-                // res.redirect('/board/read/' + idx);
-            //}
-            var idx = req.body.idx;
-            var creator_id = req.body.creator_id;
-            var title = req.body.title;
-            var content = req.body.content;
-            var passwd = req.body.passwd;
+            var idx = req.body.idx; // 75p
+            var creator_id = req.body.creator_id; // 75p
+            var title = req.body.title; // 75p
+            var content = req.body.content; // 75p
+            var passwd = req.body.passwd; // 75p
             var image_path = req.file ? req.file.path : null; // 이미지 경로 가져오기
-            var datas = [creator_id, title, content, idx, passwd, image_path];
-            console.log("updateController data : ", datas);
-            console.log(JSON.stringify(req.body));
-            updateModel.updateData(datas, (result) => {
-                if(result.affectedRows == 0){
-                    res.send("<script>alert('패스워드가 일치하지 않거나, 잘못된 요청으로 인해 변경되지 않았습니다.');history.back();</script>");
-                }
-                else{
-                res.redirect('/board'); // 바꾸어야 함
-                }
+            var datas = [creator_id, title, content, idx, passwd, image_path]; // 75p
+            console.log("updateController data : ", datas); // 75p
+            console.log(JSON.stringify(req.body)); // 75p
+            updateModel.updateData(datas, (result) => { // 75p
+                if(result.affectedRows == 0){ // 75p
+                    res.send("<script>alert('패스워드가 일치하지 않거나, 잘못된 요청으로 인해 변경되지 않았습니다.');history.back();</script>"); // 75p
+                } // 75p
+                else{ // 75p
+                res.redirect('/board/read/'+idx); // 75p
+                } // 75p
             })
         });
 };
